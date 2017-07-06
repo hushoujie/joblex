4d0f05df077794e87fd63bfaef86e6a70594878f
Berk Erol <berk.erol@boun.edu.tr>
2017-07-06 11:50:23 +0300

    ilan ekleme

---
+++ deneme/src/main/java/deneme/Ilan.java
@@ -0,0 +1,74 @@
+package deneme;
+
+public class Ilan {
+
+    private int kod;
+
+    private boolean status;
+
+    private String baslik;
+
+    private String tanim;
+
+    private String ozellikler;
+
+    private String aktivasyon;
+
+    private String kapanma;
+
+    public int getKod() {
+        return kod;
+    }
+
+    public void setKod(int kod) {
+        this.kod = kod;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getBaslik() {
+        return baslik;
+    }
+
+    public void setBaslik(String baslik) {
+        this.baslik = baslik;
+    }
+
+    public String getTanim() {
+        return tanim;
+    }
+
+    public void setTanim(String tanim) {
+        this.tanim = tanim;
+    }
+
+    public String getOzellikler() {
+        return ozellikler;
+    }
+
+    public void setOzellikler(String ozellikler) {
+        this.ozellikler = ozellikler;
+    }
+
+    public String getAktivasyon() {
+        return aktivasyon;
+    }
+
+    public void setAktivasyon(String aktivasyon) {
+        this.aktivasyon = aktivasyon;
+    }
+
+    public String getKapanma() {
+        return kapanma;
+    }
+
+    public void setKapanma(String kapanma) {
+        this.kapanma = kapanma;
+    }
+}
