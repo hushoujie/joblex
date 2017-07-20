d92dfcfc9b5714861e9cb3487e5c5b008b902e1a
Berk Erol <berk.erol@boun.edu.tr>
2017-07-20 13:57:37 +0300

    jquery tablesorter widgets

--- berkerol/src/main/resources/static/js/tablesorter.js
+++ berkerol/src/main/resources/static/js/tablesorter.js
@@ -2,12 +2,15 @@ $(function () {
     $("table").tablesorter({
         theme: "bootstrap",
         sortReset: true,
-        widthFixed: true,
         headerTemplate: '{content} {icon}',
-        widgets: ["uitheme", "filter"],
+        widgets: ["uitheme", "saveSort", "resizable", "filter", "mark", "print", "stickyHeaders"],
         widgetOptions: {
+            stickyHeaders_offset: '.navbar-fixed-top',
             filter_cssFilter: "form-control",
-            filter_saveFilters: true
+            filter_external: '.search',
+            filter_reset: '.reset_filter',
+            filter_saveFilters: true,
+            print_styleSheet: '../static/css/theme.bootstrap.css'
         }
     })
             .tablesorterPager({
@@ -16,4 +19,11 @@ $(function () {
                 // possible variables: {page}, {totalPages}, {filteredPages}, {startRow}, {endRow}, {filteredRows} and {totalRows}
                 output: '{startRow} - {endRow} / {filteredRows} ({totalRows})'
             });
+    $('.reset_sort').click(function () {
+        $('table').trigger('saveSortReset').trigger("sortReset");
+        return false;
+    });
+    $('.print').click(function () {
+        $('table').trigger('printTable');
+    });
 });
